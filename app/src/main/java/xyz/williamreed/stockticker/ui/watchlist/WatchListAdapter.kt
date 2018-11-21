17f21e4958275f5b38cc61bde0a506a8ffab6ae1
William Reed <wreed58@gmail.com>
2018-11-21 17:43:00 -0500

    diff util for updating RV

--- app/src/main/java/xyz/williamreed/stockticker/ui/watchlist/WatchListAdapter.kt
+++ app/src/main/java/xyz/williamreed/stockticker/ui/watchlist/WatchListAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import xyz.williamreed.stockticker.R
 import xyz.williamreed.stockticker.data.models.Quote
 
+// TODO: better to take in LiveData, Observable, or List here?
 class WatchListAdapter(private val data: List<Quote>) :
     RecyclerView.Adapter<WatchListAdapter.WatchListViewHolder>() {
     class WatchListViewHolder(private val watchListCardView: CardView) : RecyclerView.ViewHolder(watchListCardView) {
@@ -16,6 +18,7 @@ class WatchListAdapter(private val data: List<Quote>) :
         val primaryExchange: TextView = watchListCardView.findViewById(R.id.primaryExchange)
         val latestPrice: TextView = watchListCardView.findViewById(R.id.latestPrice)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WatchListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview_watch, parent, false) as CardView
@@ -29,6 +32,21 @@ class WatchListAdapter(private val data: List<Quote>) :
         holder.companyName.text = data[position].companyName
         holder.primaryExchange.text = data[position].primaryExchange
         holder.latestPrice.text = data[position].latestPrice.toString()
+    }
+
+    fun updateData(newData: List<Quote>) {
+        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                data[oldItemPosition].symbol == newData[newItemPosition].symbol
+
+            override fun getOldListSize() = data.size
+            override fun getNewListSize() = newData.size
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                data[oldItemPosition] == newData[newItemPosition]
+
+            // TODO: might want to override getChangePayload for some animation changes
+        }).dispatchUpdatesTo(this)
     }
 
 }
